60205dd0d9502cd0096154c379ed46dedbcacbfb
Argo triwidodo <argo.triwidodo@yahoo.co.id>
2020-07-05 20:39:07 +0700

    Fixing jcenter

---
+++ src/test/java/com/gdn/qa/module/ui/training/driver/CustomDriver.java
@@ -0,0 +1,9 @@
+package com.gdn.qa.module.ui.training.driver;
+
+import net.thucydides.core.util.EnvironmentVariables;
+import net.thucydides.core.util.SystemEnvironmentVariables;
+
+public class CustomDriver {
+    EnvironmentVariables variables = SystemEnvironmentVariables.createEnvironmentVariables();
+
+}
